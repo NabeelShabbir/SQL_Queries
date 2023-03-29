e7427553b15be780bbaca814b7d0ac14b10c16d9
NabeelShabbir <128056426+NabeelShabbir@users.noreply.github.com>
2023-03-29 23:12:45 +0500

    Update Q25.sql

--- Q25.sql
+++ Q25.sql
@@ -6,4 +6,51 @@
 	having pilot.Restrictions = 'None';
 
 	--(b)
-	
+	select * from AIRPLANE inner join HANGAR on AIRPLANE.STORED_IN = HANGAR.Number where HANGAR.H_Location = 'Islamabad';
+
+	--(c)
+	select count(distinct(Employee.ssn)) as TotalEmployee, SUM(Employee.Salary) as TotalSalary 
+	from EMPLOYEE;
+
+	--(d)
+	select seller_id,temp6.Reg#,Owner_ID as buyer_id from
+(select CORPORATION.Owner_ID as seller_id,Reg#
+from CORPORATION
+inner join
+(
+select temp3.reg#,Owner_ID,Pdate,number
+from
+(select owns.reg#,Owner_ID,Pdate,number
+from owns
+inner join (
+select * from
+(select reg#, count(Reg#) as number
+from owns
+group by Reg#) as temp
+where number > 1) as temp2
+on OWNS.Reg# = temp2.Reg#) as temp3
+inner join
+(SELECT reg#,min(pdate) as pd
+FROM owns
+group by reg#) as temp4
+on temp3.Pdate = temp4.pd and temp3.Reg#= temp4.Reg#) as temp5
+on temp5.Owner_ID = CORPORATION.Owner_ID) as temp6
+
+inner join
+(select temp3.reg#, Owner_ID
+from
+(select owns.reg#,Owner_ID,Pdate,number
+from owns
+inner join (
+select * from
+(select reg#, count(Reg#) as number
+from owns
+group by Reg#) as temp
+where number > 1) as temp2
+on OWNS.Reg# = temp2.Reg#) as temp3
+inner join
+(SELECT reg#,max(pdate) as pd
+FROM owns
+group by reg#) as temp4
+on temp3.Pdate = temp4.pd and temp3.Reg#= temp4.Reg#) as temp5
+on temp6.Reg# = temp5.Reg#;
